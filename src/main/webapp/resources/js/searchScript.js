f49a9e697401936a01418362d8bd6497dbde8c5e
Morpheus24 <matt.rudge@outlook.com>
2019-08-28 17:53:39 +1000

    Search working properly

--- src/main/webapp/resources/js/searchScript.js
+++ src/main/webapp/resources/js/searchScript.js
@@ -1,22 +1,23 @@
 function search() {
-    alert("saealdlawd");
+
     var form = $('#searchForm');
-    var searchTerm = form.find('input[name = "searchTerm"]').val();
+    var searchTerm = form.find('input[name = "searchTerm"]').val().toLocaleUpperCase();
+
 
     var table = document.getElementById("ticketTableBody");
     var rows = table.getElementsByTagName("tr");
 
-
     for (var i = 0; i < rows.length; i++) {
         var data = rows[i].getElementsByTagName("td");
-
         var foundMatch = false;
 
         if (searchTerm != null) {
             for (var j = 0; j < data.length; j++) {
-                if (data[j].contains(searchTerm)) {
+                //alert(data[j].innerText);
+                if (data[j].textContent.toLocaleUpperCase().includes(searchTerm)) {
                     foundMatch = true;
                     rows[i].style.display = "";
+                    break;
                 } else {
                     rows[i].style.display = "none";
                 }
